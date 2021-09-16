085893ab66825aed3051685bbdac6b2e82bb8c5b
kototo522 <huyejiben@gmail.com>
2021-09-16 23:18:38 +0900

    add: グリッドの作成

--- sketch.js
+++ sketch.js
@@ -4,4 +4,17 @@ function setup(){
     background(0);
     noStroke();
     fill(255);
+
+    const size = 5;
+    ellipse(50, 50, size, size);
+    ellipse(100, 50, size, size);
+    ellipse(150, 50, size, size);
+
+    ellipse(50, 100, size, size);
+    ellipse(100, 100, size, size);
+    ellipse(150, 100, size, size);
+
+    ellipse(50, 150, size, size);
+    ellipse(100, 150, size, size);
+    ellipse(150, 150, size, size);
 }
