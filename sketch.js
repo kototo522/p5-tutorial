1a7e0d5298ecf755b51038ea5d8e3f8be28e92d7
kototo522 <huyejiben@gmail.com>
2021-09-17 01:16:08 +0900

    add: 動きの追加

--- sketch.js
+++ sketch.js
@@ -1,22 +1,19 @@
 function setup(){
     // 描画領域のセットアップ
     createCanvas(600, 200);
-    background(0);
     noStroke();
     fill(255);
+    colorMode(HSB, 360, 100, 100, 255);
+}
 
+function draw(){
+    background(0, 20);
     const step = 20;
-
-    for(let y = 0; y <= height; y += step){
-        for(let x = 0; x <= width; x += step){
-            const size = random(0, 15);
-
-            const r = random(255);
-            const g = random(r);
-            const b = random(g);
-            fill(r, g, b);
-
-            ellipse(x, y, size, size);
-        }
+    const size = 5;
+    for (let y = 0; y <= height; y += step) {
+      for (let x = 0; x <= width; x += step) {
+        ellipse(x, y, size, size);
+      }
     }
+    ellipse(mouseX, mouseY, 20, 20);
 }
