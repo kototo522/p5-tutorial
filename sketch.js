47091d677e19f956136cfb2873e83060d0bb3aec
kototo522 <huyejiben@gmail.com>
2021-09-17 17:30:11 +0900

    add: マウスとの距離の追加

--- sketch.js
+++ sketch.js
@@ -7,13 +7,15 @@ function setup(){
 }
 
 function draw(){
-    background(0, 20);
+    background(0);
     const step = 20;
-    const size = 5;
+
     for (let y = 0; y <= height; y += step) {
       for (let x = 0; x <= width; x += step) {
+        const d = dist(x, y, mouseX, mouseY);
+        const size = map(sin(d * 0.05), -1, 1, 0, 10);
+        fill(map(sin(d * 0.05), -1, 1, 60, 320), 100, 100);
         ellipse(x, y, size, size);
       }
     }
-    ellipse(mouseX, mouseY, 20, 20);
 }
